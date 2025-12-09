59a5f70286ac3b4f80830be4a5d672c7c8808a12
Jean de Dieu UWINTWALI <uwintwali.umd@gmail.com>
2025-12-09 18:54:16 +0200

    add backend deployment path and storage

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080', // backend
+        target: 'https://homelyhub-hotel-reservation.onrender.com', // backend
         changeOrigin: true,
         secure: false,
       },
